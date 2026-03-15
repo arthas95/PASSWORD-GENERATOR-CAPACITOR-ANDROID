a398bcad5c752357b9ee09d365459ea6e2976ff7
Adel Sadek <ton.email@domaine.com>
2026-03-15 17:38:10 +0100

    first version password generator capacitor

---
+++ www/main.js
@@ -0,0 +1,43 @@
+let lebody = document.querySelector("body");
+const input = document.getElementById("length");
+const button = document.getElementById("generate");
+const includeLowercase = document.getElementById("includeLowercase");
+const includeUppercase = document.getElementById("includeUppercase");
+const includeNumbers = document.getElementById("includeNumbers");
+const includeSymbols = document.getElementById("includeSymbols");
+
+button.addEventListener("click", () => {
+    let length = input.value;
+    let password = generatePassword(length);
+    alert(password);
+});
+
+
+function generatePassword(length){
+    let charset = "";
+    let options = [
+        "abcdefghijklmnopqrstuvwxyz",
+         "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+         "0123456789",
+         "!@#$%^&*"
+    ];
+if (includeLowercase.checked) {
+    charset += options[0];
+}
+if (includeUppercase.checked) {
+    charset += options[1];
+}
+if (includeNumbers.checked) {
+    charset += options[2];
+}
+if (includeSymbols.checked) {
+    charset += options[3];
+}
+
+    let password = "";
+    for (let i = 0; i<length;i++){
+        let randomIndex = Math.floor(Math.random() * charset.length);
+        password += charset[randomIndex];
+    }
+    return password;
+}
